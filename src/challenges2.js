b9d68e0b3bc84ef5180d73c56b6ad93a48d8afca
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-25 12:22:06 -0300

    Adiciona funcao generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,25 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line complexity, sonarjs/cognitive-complexity, max-lines-per-function
+function generatePhoneNumber(arr) {
+  if (arr.length > 11 || arr.length < 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    let repeat = arr.filter((v) => (v === arr[i])).length;
+
+    if (arr[i] < 0 || arr[i] > 9 || repeat === 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let mask = '(xx) xxxxx-xxxx';
+
+  arr.forEach((item) => {
+    mask = mask.replace('x', item);
+  });
+
+  return mask;
 }
 
 // Desafio 12
